d2b553dc543fd56dc24a23f8c9ad44c387f03a4c
Dhanush G <dhanushg1020@gmail.com>
2026-07-17 22:29:38 +0530

    solution

---
+++ Array/54. Spiral Matrix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def spiralOrder(self, a: list[list[int]]) -> list[int]:
+        t,b=0,len(a)-1
+        l,r=0,len(a[0])-1
+        an=[]
+        while(t<=b and l<=r):
+            for i in range(l,r+1):
+                an.append(a[t][i])
+            t+=1
+            for i in range(t,b+1):
+                an.append(a[i][r])
+            r-=1
+            if t<=b:
+                for i in range(r,l-1,-1):
+                    an.append(a[b][i])
+                b-=1
+            if l<=r:
+                for i in range(b,t-1,-1):
+                    an.append(a[i][l])
+                l+=1
+        return an
+                
