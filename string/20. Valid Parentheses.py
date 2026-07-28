47ee2e4762f57e882d02c0d6c98461d2b274b438
Dhanush G <dhanushg1020@gmail.com>
2026-07-28 20:37:39 +0530

    solution

---
+++ string/20. Valid Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')':'(', ']':'[', '}':'{'}
+
+        for i in s:
+            if i in '({[':
+                stack.append(i)
+            else:
+                if not stack or stack.pop() != pairs[i]:
+                    return False
+
+        return not stack
